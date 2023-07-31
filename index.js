3459c86c34dc8069a790c4f82a365862a591f247
KeenoFourie <keenofourie614@gmail.com>
2023-07-31 14:33:08 +0200

    update

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 const {writeFile, readFile, appendFile} = require('fs')
 
-readFile('./data.txt', 'utf-8', (err, data)=> {
+
+writeFile('./data.txt', 'Hello World👋', 'utf-8', (err)=> {
     if(!err)
-        console.log(data);
-        else
-    console.log('An error occurred');
+        console.log('Hello World👋');
+    else
+        console.log('Try again!');
 })
+
+writeFile('./data.txt', 'Goodbye World🫂', 'utf-8', (err)=> {
+    if(!err)
+        console.log('Goodbye World🫂');
+    else
+        console.log('Try again!');
+})
+
+// It is placed on the next line
+
+appendFile('./data.txt', 'This is how we do it!💃🕺', 'utf-8', (err)=> {
+    if(!err)
+        console.log('This is how we do it!💃🕺');
+    else
+        console.log('Try again!');
+})
+
+
+
+// readFile('./data.txt', 'utf-8', (err, data)=> {
+//     if(!err)
+//         console.log(data);
+//         else
+//     console.log('An error occurred');
+// })
